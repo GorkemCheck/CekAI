6ac963f194827ccb84f9d2ab5ac7579f736e7d35
Gorkem <g.cek@student.rug.nl>
2024-08-09 15:21:29 +0200

    CekAI V2

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
     if(form.prompt) {
       try {
         setGeneratingImage(true);
-        const response = await fetch('http://localhost:8080/api/v1/stability', {
+        const response = await fetch('https://cekai.onrender.com/api/v1/stability', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -50,7 +50,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post',
+        const response = await fetch('https://cekai.onrender.com/api/v1/post',
         { method: 'POST',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({...form})
